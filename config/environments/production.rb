303fa4fb3372465b6aed7407a59d22043dc78180
Jimmy <jimmylorunning@gmail.com>
2015-01-21 10:38:36 -0500

    Install Devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ OmNomNom::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'vast-refuge-7886.herokuapp.com', port: 80 }
 end
